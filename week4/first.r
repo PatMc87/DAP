36167b83af2be690538de0e8da2348ba82662675
PatMc87 <patrickmcnamara_2@msn.com>
2017-10-04 20:40:23 +0100

    first R script

---
+++ week4/first.r
@@ -0,0 +1,16 @@
+# correlation coefficient analysis
+
+x <- c(2,3,4,1,6,5,4,9,6,7)
+
+# y is the dependent variable
+
+y <- c(5,6,7,8,9,8,2,1,2,3)
+
+res <- cor(x,y)
+
+print(res)
+
+# correlation coefficient calculation
+# is order dependent and moving the order
+# around affects the coefficient calculation
+
